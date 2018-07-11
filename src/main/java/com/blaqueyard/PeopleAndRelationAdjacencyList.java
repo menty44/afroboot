39cd1f8ccbb381777a0a6cbe2415abd2205743cf
Fredrick Oluoch <menty44@gmail.com>
2018-07-11 22:44:37 +0300

    bugs in relations code

---
+++ src/main/java/com/blaqueyard/PeopleAndRelationAdjacencyList.java
@@ -0,0 +1,13 @@
+package com.blaqueyard;/**
+ * Created by admin on 7/11/18.
+ */
+
+/**
+ * Fredrick Oluoch
+ * http://www.blaqueyard.com
+ * 0720106420 | 0722508906
+ * email: menty44@gmail.com
+ */
+
+public class PeopleAndRelationAdjacencyList {
+}
